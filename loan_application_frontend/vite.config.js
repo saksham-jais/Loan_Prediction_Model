f7d76c985d9c283a6693c347861f851ebe368ce7
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2025-08-08 20:01:35 +0530

    reverting pre changes for index.html in vite config files

--- loan_application_frontend/vite.config.js
+++ loan_application_frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: 'loan_application_frontend', // <--  frontend folder as root
   plugins: [react(), tailwindcss()],
 })
